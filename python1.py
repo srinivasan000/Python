14032171daabe2032dddb7491a2817e20940a3a0
srinivasan000 <srinivasans2425s@gmail.com>
2022-01-29 18:38:26 +0530

    print function python

---
+++ python1.py
@@ -0,0 +1,13 @@
+#single line comment
+""" multiple line comment """
+############### print() function ###############
+
+print("print(\"Hello world\")")
+print("my name is " + "srinivasan")
+print(5+5)
+print("my name is","srinivasan")
+print("my name is","srinivasan",sep=" : ",end=".\n")
+print("""1.line1
+2.line2
+3.line3  
+ """);
